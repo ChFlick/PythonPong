54fb73b76ead6cb31d5d39eba7e1d74b75962bcf
Christoph Flick <christophflick@gmx.de>
2017-09-26 22:09:08 +0200

    Game initally completed

---
+++ PongGame.py
@@ -0,0 +1,22 @@
+from random import randint
+
+from kivy.properties import ObjectProperty
+from kivy.uix.widget import Widget
+from kivy.vector import Vector
+
+
+class PongGame(Widget):
+    ball = ObjectProperty(None)
+
+    def serve_ball(self):
+        self.ball.center = self.center
+        self.ball.velocity = Vector(4, 0).rotate(randint(0, 360))
+
+    def update(self, dt):
+        self.ball.move()
+
+        if(self.ball.y < 0) or (self.ball.top > self.height):
+            self.ball.velocity_y *= -1
+
+        if (self.ball.x < 0) or (self.ball.right > self.width):
+            self.ball.velocity_x *= -1
