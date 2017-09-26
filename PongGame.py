14a8b01d01579dc8d53cb7ce3346a33b58192048
Christoph Flick <christophflick@gmx.de>
2017-09-26 22:29:05 +0200

    reset paddle position on score

--- PongGame.py
+++ PongGame.py
@@ -2,7 +2,6 @@ from random import randint
 
 from kivy.properties import ObjectProperty
 from kivy.uix.widget import Widget
-from kivy.vector import Vector
 
 
 class PongGame(Widget):
@@ -10,32 +9,41 @@ class PongGame(Widget):
     player1 = ObjectProperty(None)
     player2 = ObjectProperty(None)
 
+    def update(self, dt):
+        self.ball.move()
+
+        self.__check_player_hit()
+        self.__check_wall_hit()
+        self.__check_score()
+
     def serve_ball(self, vel=(4 if randint(0, 1) is 0 else -4, 0)):
         self.ball.center = self.center
         self.ball.velocity = vel
 
-    def update(self, dt):
-        self.ball.move()
+    def reset_paddles(self):
+        posY = self.height / 2
+        self.player1.reset_paddle(posY)
+        self.player2.reset_paddle(posY)
 
-        self.__checkPlayerHit()
-        self.__checkWallHit()
-        self.__checkScore()
+    def restart(self):
+        self.serve_ball()
+        self.reset_paddles()
 
-    def __checkPlayerHit(self):
+    def __check_player_hit(self):
         self.player1.bounce_ball(self.ball)
         self.player2.bounce_ball(self.ball)
 
-    def __checkWallHit(self):
+    def __check_wall_hit(self):
         if (self.ball.y < 0) or (self.ball.top > self.height):
             self.ball.velocity_y *= -1
 
-    def __checkScore(self):
+    def __check_score(self):
         if self.ball.x < self.x:
             self.player2.score += 1
-            self.serve_ball(vel=(-4, 0))
+            self.restart()
         elif self.ball.x > self.width:
             self.player1.score += 1
-            self.serve_ball(vel=(4, 0))
+            self.restart()
 
     '''move the player paddles by touching the sides'''
     def on_touch_move(self, touch):
--- PongPaddle.py
+++ PongPaddle.py
@@ -6,10 +6,13 @@ from kivy.vector import Vector
 class PongPaddle(Widget):
     score = NumericProperty(0)
 
+    def reset_paddle(self, window_height):
+        self.y = window_height - self.height / 2
+
     def bounce_ball(self, ball):
         if self.collide_widget(ball):
             vx, vy = ball.velocity
-            offset = (ball.center_y - self.center_y) / (self.height / 8)
+            offset = (ball.center_y - self.center_y) / (self.height / 4)
             bounced = Vector(-1 * vx, vy)
             vel = bounced * 1.1
             ball.velocity = vel.x, vel.y + offset
